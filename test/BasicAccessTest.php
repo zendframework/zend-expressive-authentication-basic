496a777f237269cca52c94ccb2064c2445788663
Matthew Weier O'Phinney <matthew@zend.com>
2017-11-09 08:28:30 -0600

    Wrap long lines, per phpcs

--- test/BasicAccessTest.php
+++ test/BasicAccessTest.php
@@ -2,7 +2,8 @@
 /**
  * @see       https://github.com/zendframework/zend-expressive-authentication-basic for the canonical source repository
  * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   https://github.com/zendframework/zend-expressive-authentication-basic/blob/master/LICENSE.md New BSD License
+ * @license   https://github.com/zendframework/zend-expressive-authentication-basic/blob/master/LICENSE.md
+ *     New BSD License
  */
 
 namespace ZendTest\Expressive\Authentication\Basic;
