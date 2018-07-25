f7392377dec4ee638c1fafd5f919d685120b2932
Micha Kühn <kuehn.michael@icloud.com>
2018-07-25 11:46:39 +0200

    #8 test with dataProvider shouldn't have static assertion

--- test/BasicAccessTest.php
+++ test/BasicAccessTest.php
@@ -107,7 +107,7 @@ class BasicAccessTest extends TestCase
 
         $user = $basicAccess->authenticate($this->request->reveal());
         $this->assertInstanceOf(UserInterface::class, $user);
-        $this->assertEquals('Aladdin', $user->getIdentity());
+        $this->assertEquals($username, $user->getIdentity());
     }
 
     public function testIsAuthenticatedWithNoCredential()
